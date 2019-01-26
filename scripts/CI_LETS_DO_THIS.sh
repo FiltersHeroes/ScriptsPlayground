2072ac1874057f06b8982df53392a140ff57a9f2
hawkeye116477 <hawkeye116477@gmail.com>
2019-01-26 12:04:30 +0100

    Test
    Na początek

--- scripts/CI_LETS_DO_THIS.sh
+++ scripts/CI_LETS_DO_THIS.sh
@@ -4,8 +4,8 @@ sciezka=$(dirname "$0")
 
 cd $sciezka/..
 
-wget -O ./PPB.txt https://raw.githubusercontent.com/PolishFiltersTeam/PolishAnnoyanceFilters/master/PPB.txt
+wget -O ./PAF_backgrounds_self-advertising.txt https://raw.githubusercontent.com/PolishFiltersTeam/PolishAnnoyanceFilters/master/PAF_backgrounds_self-advertising.txt
 
-./scripts/expired_domains.sh ./PPB.txt
+./scripts/expired_domains.sh $sciezka/../PPB.txt
 
 git push https://PolishJarvis:${GIT_TOKEN}@${TRAVIS_REPO_SLUG} HEAD:master > /dev/null 2>&1
