9fbb70c0455fcfb5347f7a35c20462e8a91021c6
hawkeye116477 <hawkeye116477@gmail.com>
2019-01-26 12:06:28 +0100

    Fix

--- scripts/CI_LETS_DO_THIS.sh
+++ scripts/CI_LETS_DO_THIS.sh
@@ -6,6 +6,6 @@ cd $sciezka/..
 
 wget -O ./PAF_backgrounds_self-advertising.txt https://raw.githubusercontent.com/PolishFiltersTeam/PolishAnnoyanceFilters/master/PAF_backgrounds_self-advertising.txt
 
-./scripts/expired_domains.sh $sciezka/../PPB.txt
+./scripts/expired_domains.sh $sciezka/../PAF_backgrounds_self-advertising.txt
 
-git push https://PolishJarvis:${GIT_TOKEN}@${TRAVIS_REPO_SLUG} HEAD:master > /dev/null 2>&1
+git push https://PolishJarvis:${GIT_TOKEN}@github.com/PolishFiltersTeam/ExpiredDomainsFilterListsPlayground.git HEAD:master > /dev/null 2>&1
