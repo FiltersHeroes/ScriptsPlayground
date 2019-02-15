20812e9b73f9e34e552e3cdcf2bab82980e49105
hawkeye116477 <hawkeye116477@gmail.com>
2019-02-15 16:39:08 +0100

    Poprawki
    Teraz nierozpoznane będą dodatkowo sprawdzane przez curl

--- scripts/expired_domains.sh
+++ scripts/expired_domains.sh
@@ -47,16 +47,32 @@ do
     fi
 done
 
-awk '{gsub("www.", "");print}' $TEMPORARY.2 >> $TEMPORARY.3
+awk '{gsub("ww..", "");print}' $TEMPORARY.2 >> $TEMPORARY.3
+sort -u -o $TEMPORARY.3
 
 $MAIN_PATH/scripts/domain-check-2.sh -f $TEMPORARY.3 | tee $TEMPORARY.4
 sed '/Expired/!d' $TEMPORARY.4 | cut -d' ' -f1 > $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
-sed '/Unknown/!d' $TEMPORARY.4 | cut -d' ' -f1 > $MAIN_PATH/expired-domains/$FILTERLIST-unknown.txt
-sort -u -o $FILTERLIST-expired.txt
-sort -u -o $FILTERLIST-unknown.txt
+
+sed '/Unknown/!d' $TEMPORARY.4 | cut -d' ' -f1 >> $TEMPORARY.5
+
+for ips in `cat $TEMPORARY.5`
+do
+    status_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $ips)
+    if [ $status_code -ne "200" ]
+    then
+        echo  "$ips $status_code" >> $MAIN_PATH/expired-domains/$FILTERLIST-unknown.txt
+    else
+        echo "Test"
+    fi
+done
+
+
+sort -u -o $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+sort -u -o $MAIN_PATH/expired-domains/$FILTERLIST-unknown.txt
 rm -rf $TEMPORARY
 rm -rf $TEMPORARY.2
 rm -rf $TEMPORARY.3
 rm -rf $TEMPORARY.4
+rm -rf $TEMPORARY.5
 
 done
