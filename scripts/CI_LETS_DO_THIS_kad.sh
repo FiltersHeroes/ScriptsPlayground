37308321658caf3aedc1fbdbe64417f2dc158d3a
hawkeye116477 <hawkeye116477@gmail.com>
2019-02-21 16:50:25 +0100

    Zobaczymy co tym razem Travis powie... [ci skip]
    Bo GL coś sporo unknown wypluwa, jakieś limity? Opóźnianie jakoś za bardzo nie pomaga, jakby tracił połączenie po czasie, bo nawet czasem jest „no connection"

--- scripts/CI_LETS_DO_THIS_kad.sh
+++ scripts/CI_LETS_DO_THIS_kad.sh
@@ -5,9 +5,12 @@ sciezka=$(dirname "$0")
 cd $sciezka/..
 
 wget https://raw.githubusercontent.com/PolishFiltersTeam/KAD/master/KAD.txt
+wget https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/sections/hostsplus.txt
 
-./scripts/expired_domains.sh $sciezka/../KAD.txt
+./scripts/expired_domains.sh $sciezka/../KAD.txt $sciezka/../hostsplus.txt
 rm -r ./KAD.txt
+rm -r ./hostsplus.txt
+
 git add --all
 git commit -m "Check [ci skip]"
 git push https://PolishJarvis:${GIT_TOKEN}@github.com/PolishFiltersTeam/ExpiredDomainsFilterListsPlayground.git HEAD:master > /dev/null 2>&1
