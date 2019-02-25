6daac90ee8bbf35e3c004cfa15bf4e469ff10023
hawkeye116477 <hawkeye116477@gmail.com>
2019-02-25 17:13:22 +0100

    Zamieniamy subdomeny na domeny [ci skip]
    Na razie wersja alpha, trochę słabo, bo trzeba „robić wyjątki"

--- scripts/expired_domains.sh
+++ scripts/expired_domains.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# v1.4.6
+# v1.5
 
 for i in "$@"; do
 pageComma=$(pcregrep -o1 '^([^\/\*\|\@\"\!]*?)#\@?#\K.*' $i)
@@ -52,24 +52,29 @@ sed -i "s/^www[0-9]\.//" $TEMPORARY.2
 sed -i "s/^www\.//" $TEMPORARY.2
 sort -u -o $TEMPORARY.2 $TEMPORARY.2
 
-$MAIN_PATH/scripts/domain-check-2.sh -f $TEMPORARY.2 | tee $TEMPORARY.3
-sed '/Expired/!d' $TEMPORARY.3 | cut -d' ' -f1 > $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+# Zamieniamy subdomeny na domeny (alpha)
+# https://ubuntuforums.org/showthread.php?t=873034&s=99fb8190182be62fbf8f81352b2fa4fa&p=5477397#post5477397
+awk -F. '{if ($(NF-1) == "co"|| $(NF-1) == "com" || $(NF-1) == "net" || $(NF-1) == "edu" || $(NF-1) == "org" ) printf $(NF-2)"."; printf $(NF-1)"."$(NF)"\n";}' $TEMPORARY.2 >> $TEMPORARY.3
 
-sed '/Unknown/!d' $TEMPORARY.3 | cut -d' ' -f1 >> $TEMPORARY.4
 
-for ips in `cat $TEMPORARY.4`
+$MAIN_PATH/scripts/domain-check-2.sh -f $TEMPORARY.3 | tee $TEMPORARY.4
+sed '/Expired/!d' $TEMPORARY.4 | cut -d' ' -f1 > $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+
+sed '/Unknown/!d' $TEMPORARY.4 | cut -d' ' -f1 >> $TEMPORARY.5
+
+for ips in `cat $TEMPORARY.5`
 do
     status_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $ips)
     if [ $status_code -ne "200" ]
     then
-        echo  "$ips $status_code" >> $TEMPORARY.5
+        echo  "$ips $status_code" >> $TEMPORARY.6
     else
         echo "Test"
     fi
 done
 
 touch $MAIN_PATH/expired-domains/$FILTERLIST-unknown.txt
-mv $TEMPORARY.5 $MAIN_PATH/expired-domains/$FILTERLIST-unknown.txt
+mv $TEMPORARY.6 $MAIN_PATH/expired-domains/$FILTERLIST-unknown.txt
 
 sort -u -o $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
 sort -u -o $MAIN_PATH/expired-domains/$FILTERLIST-unknown.txt $MAIN_PATH/expired-domains/$FILTERLIST-unknown.txt
@@ -78,5 +83,6 @@ rm -rf $TEMPORARY.2
 rm -rf $TEMPORARY.3
 rm -rf $TEMPORARY.4
 rm -rf $TEMPORARY.5
+rm -rf $TEMPORARY.6
 
 done
