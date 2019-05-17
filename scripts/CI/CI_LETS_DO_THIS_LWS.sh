4635805f18f4abccf1c1b5c2cea80b0fc04805bd
hawkeye116477 <hawkeye116477@gmail.com>
2019-05-17 21:18:54 +0200

    Improve script

--- scripts/CI/CI_LETS_DO_THIS_LWS.sh
+++ scripts/CI/CI_LETS_DO_THIS_LWS.sh
@@ -12,6 +12,8 @@ sed -i -r "s|http(s)?:\/\/||" $LWS
 sed -i -r "s|^|\|\||" $LWS
 sed -i "s|[/]||" $LWS
 sed -i 's|$|\^|' $LWS
+sed -i "s/^www[0-9]\.//" $LWS
+sed -i "s/^www\.//" $LWS
 wget https://raw.githubusercontent.com/PolishFiltersTeam/KAD/master/sections/podejrzane_inne_oszustwa.txt
 sort -u -o ./podejrzane_inne_oszustwa.txt ./podejrzane_inne_oszustwa.txt
 sort -u -o $LWS $LWS
@@ -23,13 +25,21 @@ mv $LWS.2 $SCRIPT_PATH/LWS_temp.txt
 
 EXPIRED=$MAIN_PATH/expired-domains/LWS_temp-expired.txt
 UNKNOWN=$MAIN_PATH/expired-domains/LWS_temp-unknown.txt
+
 if [ -f "$EXPIRED" ]; then
     comm -2 -3 $SCRIPT_PATH/LWS_temp.txt $EXPIRED >> $MAIN_PATH/LWS/podejrzane_LWS.txt
     rm -r $SCRIPT_PATH/LWS_temp.txt
-else
-    mv $SCRIPT_PATH/LWS_temp.txt $MAIN_PATH/LWS/podejrzane_LWS.txt
+    mv $MAIN_PATH/LWS/podejrzane_LWS.txt $SCRIPT_PATH/LWS_temp.txt
 fi
 
+if [ -f "$UNKNOWN"]; then
+    comm -2 -3 $SCRIPT_PATH/LWS_temp.txt $UNKNOWN >> $MAIN_PATH/LWS/podejrzane_LWS.txt
+    rm -r $SCRIPT_PATH/LWS_temp.txt
+fi
+
+if [ ! -f "$MAIN_PATH/LWS/podejrzane_LWS.txt" ]; then
+    mv $SCRIPT_PATH/LWS_temp.txt $MAIN_PATH/LWS/podejrzane_LWS.txt
+fi
 
 if [ -f "$EXPIRED" ]; then
     rm -r $EXPIRED
