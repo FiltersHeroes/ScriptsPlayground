054fa4fadbe31962a5f31b21039d8397b884c7da
hawkeye116477 <hawkeye116477@gmail.com>
2019-06-13 18:28:04 +0200

    Test łączenia

--- scripts/TRAVIS_LETS_DO_THIS.sh
+++ scripts/TRAVIS_LETS_DO_THIS.sh
@@ -3,9 +3,9 @@
 ost_commit_katalog=$(dirname $(git diff-tree --no-commit-id --name-only -r master))
 ost_commit_plik=$(git diff-tree --no-commit-id --name-only -r master)
 
-if [ "$ost_commit_katalog" == "Test" ]; then
+if [ "$ost_commit_katalog" == "VICHS_Test" ]; then
     glowna_lista="Test.txt"
-elif [ "$ost_commit_katalog" == "Test/uBO_AG" ]; then
+elif [ "$ost_commit_katalog" == "VICHS_Test/uBO_AG" ]; then
     glowna_lista="Test.txt Test_uBO_AG.txt"
 fi
 
