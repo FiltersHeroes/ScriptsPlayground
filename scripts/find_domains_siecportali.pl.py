c775932f92096235fd7cd6f107ccf5f011b0f8af
hawkeye116477 <hawkeye116477@gmail.com>
2019-06-18 10:13:49 +0200

    Skrypt do sieci portali [ci skip]

---
+++ scripts/find_domains_siecportali.pl.py
@@ -0,0 +1,27 @@
+# import libraries
+from bs4 import BeautifulSoup
+import certifi
+import urllib3
+import re
+
+# specify the url
+quote_page = 'http://siecportali.pl/realizacje'
+
+# query the website and return the html to the variable ‘page’
+http = urllib3.PoolManager(
+    cert_reqs='CERT_REQUIRED',
+    ca_certs=certifi.where()
+)
+page = http.request('GET', quote_page)
+
+# parse the html using beautiful soup and store in variable `soup`
+soup = BeautifulSoup(page.data, "html5lib")
+
+data = soup.find_all('div', class_="portal-logos")
+
+for div in data:
+    links = div.find_all('a')
+    for a in links:
+        a['href'] = re.sub('http(s)?:\/\/', "", a['href'])
+        a['href'] = re.sub('[\/]$', "", a['href'])
+        print(a['href'])
