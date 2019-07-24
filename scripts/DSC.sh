983bf8f696f2c22e8c8d19405fbb547c0704dde2
hawkeye116477 <hawkeye116477@gmail.com>
2019-07-24 12:16:29 +0200

    Nowe wypiera stare... [ci skip]
    Usunięto zbędne dla nas informacje o rejestratorze, dodano nowe statusy niebazujące na datach, a oprócz tego skrypt powinien być bardziej ogólny (staramy się jakby nie wyciągać info o dacie dla każdego TLD w innym sposób)  :smile:

---
+++ scripts/DSC.sh
@@ -0,0 +1,349 @@
+#!/usr/bin/env bash
+
+#
+# Program: DSC <Domain Status Check>
+#
+# Author: hawkeye116477 < hawkeye116477 at gmail dot com >
+#
+# Based on domain-check-2.sh (https://github.com/click0/domain-check-2/blob/master/domain-check-2.sh)
+# and domains.sh script (https://github.com/tdulcet/Remote-Servers-Status/blob/master/domains.sh).
+# Many thanks for nixcraft (https://github.com/nixcraft), tdulcet (https://github.com/tdulcet), click0 (https://github.com/click0)
+# and all contributors of domain-check-2 script (https://github.com/click0/domain-check-2/graphs/contributors) !
+#
+#
+# Current Version: 1.0
+#
+# Revision History:
+#
+#  Version 1.0
+#    Initial Release
+#
+#
+# Purpose:
+#  DSC checks to see what domain has status (expired, suspended, book blocked, etc).
+#  DSC can be run in interactive and batch mode, and provides faciltities
+#  to alarm if a domain is about to expire.
+#
+# License:
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#
+# Requirements:
+#   Requires whois
+#
+# Installation:
+#   Copy the shell script to a suitable location
+#
+# Usage:
+#  Refer to the usage() sub-routine, or invoke DSC
+#  with the "-h" option.
+#
+#
+#
+
+PATH=/bin:/usr/bin:/usr/local/bin:/usr/local/ssl/bin:/usr/sfw/bin
+export PATH
+
+# Who to page when an expired domain is detected (cmdline: -e)
+ADMIN="sysadmin@mydomain.com"
+
+# Number of days in the warning threshhold  (cmdline: -x)
+WARNDAYS=30
+
+# If QUIET is set to TRUE, don't print anything on the console (cmdline: -q)
+QUIET="FALSE"
+
+# Don't send emails by default (cmdline: -a)
+ALARM="FALSE"
+
+# Don't show the version of the script by default (cmdline: -V)
+VERSIONENABLE="FALSE"
+
+# Don't show debug information by default (cmdline: -vv)
+VERBOSE="FALSE"
+
+# Whois server to use (cmdline: -s)
+WHOIS_SERVER="whois.internic.org"
+
+# Location of system binaries
+AWK=`which awk`
+WHOIS=`which whois`
+DATE=`which date`
+CUT=`which cut`
+GREP=`which grep`
+TR=`which tr`
+MAIL=`which mail`
+CURL=`which curl`
+
+# Version of the script
+VERSION=$(${AWK} -F': ' '/^# Current Version:/ {print $2; exit}' $0)
+
+# Place to stash temporary files
+WHOIS_TMP="/var/tmp/whois.$$"
+WHOIS_2_TMP="/var/tmp/whois_2.$$"
+
+
+##################################################################
+# Purpose: Access whois data to grab the expiration date
+# Arguments:
+#   $1 -> Domain to check
+##################################################################
+check_domain_status()
+{
+    # Avoid WHOIS LIMIT EXCEEDED - slowdown our whois client by adding 3 sec
+    sleep 3
+    # Save the domain since set will trip up the ordering
+    DOMAIN=${1}
+    TLDTYPE=$(echo ${DOMAIN} | ${AWK} -F. '{print tolower($NF);}')
+    if [ "${TLDTYPE}"  == "" ];
+    then
+        TLDTYPE=$(echo ${DOMAIN} | ${AWK} -F. '{print tolower($(NF-1));}')
+    fi
+    if [ "${TLDTYPE}"  == "ua" -o "${TLDTYPE}"  == "pl" ];
+    then
+        SUBTLDTYPE=$(echo ${DOMAIN} | ${AWK} -F. '{print tolower($(NF-1)"."$(NF));}')
+    fi
+
+    # Invoke whois to find the domain expiration date
+    #${WHOIS} -h ${WHOIS_SERVER} "=${1}" > ${WHOIS_TMP}
+    # Let whois select server
+
+    WHS="$(${WHOIS} -h "whois.iana.org" "${TLDTYPE}" | ${GREP} 'whois:' | ${AWK} '{print $2}')"
+
+    if [ "${TLDTYPE}" == "aero" ];
+    then
+        ${WHOIS} -h whois.aero "${1}" | env LC_CTYPE=C LC_ALL=C ${TR} -d "\r" > ${WHOIS_TMP}
+    elif [ "${TLDTYPE}" == "cn" ];
+    then
+       ${WHOIS} -h whois.cnnic.cn "${1}" | env LC_CTYPE=C LC_ALL=C ${TR} -d "\r" > ${WHOIS_TMP}
+    elif [ "${TLDTYPE}" == "pl" ] && [ "${SUBTLDTYPE}" != "co.pl" ];
+    then
+       ${WHOIS} -h whois.dns.pl "${1}" | env LC_CTYPE=C LC_ALL=C ${TR} -d "\r" > ${WHOIS_TMP}
+    elif [ "${SUBTLDTYPE}" == "co.pl" ];
+    then
+       ${WHOIS} -h whois.co.pl "${1}" | env LC_CTYPE=C LC_ALL=C ${TR} -d "\r" > ${WHOIS_TMP}
+    elif [ "${TLDTYPE}" == "is" ];
+    then
+       ${WHOIS} -h whois.isnic.is "${1}" | env LC_CTYPE=C LC_ALL=C ${TR} -d "\r" > ${WHOIS_TMP}
+    elif [ "${TLDTYPE}" == "stream" ];
+    then
+        ${WHOIS} -h whois.nic.stream "${1}" | env LC_CTYPE=C LC_ALL=C ${TR} -d "\r" > ${WHOIS_TMP}
+    else
+        ${WHOIS} -h ${WHS} "${1}" | env LC_CTYPE=C LC_ALL=C ${TR} -d "\r" > ${WHOIS_TMP}
+    fi
+
+    if [ "${TLDTYPE}" == "kz" ];
+    then
+       ${CURL} -s "https://api.ps.kz/kzdomain/domain-whois?username=test&password=test&input_format=http&output_format=get&dname=${1}" \
+        | env LC_CTYPE=C LC_ALL=C ${TR} -d "\r" > ${WHOIS_2_TMP}
+    fi
+
+    removed=$(cat ${WHOIS_TMP} | ${GREP} "The queried object does not exist: previous registration")
+
+    # The whois Expiration data should resemble the following: "Expiration Date: 09-may-2008-16:00:00-CEST"
+    export LC_ALL=en_US.UTF-8
+    if adate=$(cat ${WHOIS_TMP} | ${GREP} -i 'expiration\|expires\|expiry\|renewal\|expire\|paid-till\|valid until\|exp date\|vencimiento'); then
+			adate=$(echo "$adate" | head -n 1 | sed -n 's/^[^]:]\+[]:][.[:blank:]]*//p')
+			adate=${adate%.}
+			if date=$(${DATE}  -u -d "$adate" 2>&1) || date=$(${DATE}  -u -d "${adate//./-}" 2>&1) || date=$(${DATE}  -u -d "${adate//.//}" 2>&1) || date=$(${DATE} -u -d "$(echo "${adate//./-}" | awk -F'[/-]' '{for(i=NF;i>0;i--) printf "%s%s",$i,(i==1?"\n":"-")}')" 2>&1); then
+				DOMAINDATE=$(${DATE} -d "$date" +"%d-%b-%Y-%T-%Z")
+				sec=$(( $(${DATE} -d "$date" +%s) - $(${DATE} -d "$NOW" +%s) ))
+				DOMAINDIFF=$(( sec / 86400 ))
+			else
+				DOMAINDATE="Unknown ($adate)" # date="Error: Could not input domain expiration date ($adate)."
+			fi
+    elif [ "${TLDTYPE}" == "kz" ]; # for .kz @click0 2019/02/23
+    then
+        adate=`${GREP} -A 1 "expire" ${WHOIS_2_TMP} | ${GREP} "utc" | ${AWK} -F\" '{print $4;}'`
+        DOMAINDATE=$(${DATE} -d "${adate}" +"%d-%b-%Y-%T-%Z")
+        sec=$(( $(${DATE} -d "$adate" +%s) - $(${DATE} -d "$NOW" +%s) ))
+        DOMAINDIFF=$(( sec / 86400 ))
+    elif [ ! -z "$removed" ]
+    then
+        adate=`${GREP} -oP -m 1 "was purged on \K.*" ${WHOIS_TMP} | ${AWK} -F\" '{print $1;}'`
+        DOMAINDATE=$(${DATE} -d "${adate}" +"%d-%b-%Y-%T-%Z")
+        sec=$(( $(${DATE} -d "$adate" +%s) - $(${DATE} -d "$NOW" +%s) ))
+        DOMAINDIFF=$(( sec / 86400 ))
+	else
+        DOMAINDATE="Unknown" # date="Error: Could not get domain expiration date."
+        DOMAINDIFF="Unknown"
+    fi
+
+    book_blocked=$(cat ${WHOIS_TMP} | ${GREP} "after release from the queue, available for registration")
+    suspended=$(cat ${WHOIS_TMP} | ${GREP} "is undergoing proceeding")
+    active=$(cat ${WHOIS_TMP} | ${GREP} "Status: [[:space:]]*active")
+    free=$(cat ${WHOIS_TMP} | ${GREP} "is free")
+    suspended_reserved=$(cat ${WHOIS_TMP} | ${GREP} "cancelled, suspended, refused or reserved at the Dot TK Registry" )
+    redemption_period=$(cat ${WHOIS_TMP} | ${GREP} "redemptionPeriod" )
+
+    if [ ! -z "$removed" ]
+    then
+        prints "${DOMAIN}" "Removed" "${DOMAINDATE}" "${DOMAINDIFF}"
+    elif [ ! -z "$suspended_reserved" ]
+    then
+        prints "${DOMAIN}" "Suspended_or_reserved" "${DOMAINDATE}" "${DOMAINDIFF}"
+    elif [ ! ${DOMAINDIFF} == "Unknown" ] && [ ${DOMAINDIFF} -lt 0 ]
+    then
+        prints "${DOMAIN}" "Expired" "${DOMAINDATE}" "${DOMAINDIFF}"
+    elif [ ! ${DOMAINDIFF} == "Unknown" ] && [ ${DOMAINDIFF} -lt ${WARNDAYS} ]
+    then
+        prints "${DOMAIN}" "Expiring" "${DOMAINDATE}" "${DOMAINDIFF}"
+    elif [ ! -z "${free}" ]
+    then
+        prints "${DOMAIN}" "Free" "${DOMAINDATE}" "${DOMAINDIFF}"
+    elif [ ! -z "${redemption_period}" ]
+    then
+        prints "${DOMAIN}" "Redemption_period" "${DOMAINDATE}" "${DOMAINDIFF}"
+    elif [ ${DOMAINDATE} == "Unknown" ] || [ ${DOMAINDATE} == "Unknown ($adate)" ] && [ -z "$active" ]
+    then
+        prints "${DOMAIN}" "Unknown" "${DOMAINDATE}" "${DOMAINDIFF}"
+    elif [ ! -z "$book_blocked" ]
+    then
+        prints "${DOMAIN}" "Book_blocked" "${DOMAINDATE}" "${DOMAINDIFF}"
+    elif [ ! -z "$suspended" ]
+    then
+        prints "${DOMAIN}" "Suspended" "${DOMAINDATE}" "${DOMAINDIFF}"
+    else
+        prints "${DOMAIN}" "Valid" "${DOMAINDATE}"  "${DOMAINDIFF}"
+    fi
+}
+
+####################################################
+# Purpose: Print a heading with the relevant columns
+# Arguments:
+#   None
+####################################################
+print_heading()
+{
+    if [ "${QUIET}" != "TRUE" ]
+    then
+        printf "\n%-35s %-21s %-31s %-5s\n" "Domain" "Status" "Expires" "Days Left"
+        echo "----------------------------------- --------------------- ------------------------------- ---------"
+    fi
+}
+
+#####################################################################
+# Purpose: Print a line with the expiraton interval
+# Arguments:
+#   $1 -> Domain
+#   $2 -> Status of domain (e.g., expired or valid)
+#   $3 -> Date when domain will expire
+#   $4 -> Days left until the domain will expire
+#####################################################################
+prints()
+{
+    if [ "${QUIET}" != "TRUE" ]
+    then
+        MIN_DATE=$(echo $3 | ${AWK} '{ print $1, $2, $4 }')
+        printf "%-35s %-21s %-31s %-5s\n" "$1" "$2" "$MIN_DATE" "$4"
+    fi
+}
+
+##########################################
+# Purpose: Describe how the script works
+# Arguments:
+#   None
+##########################################
+usage()
+{
+    echo "Usage: $0 [ -e email ] [ -x expir_days ] [ -s whois server ] [ -q ] [ -a ] [ -h ] [ -v ] [ -V ]"
+    echo "	  {[ -d domain_namee ]} || { -f domainfile}"
+    echo ""
+    echo "  -a               : Send a warning message through email"
+    echo "  -d domain        : Domain to analyze (interactive mode)"
+    echo "  -e email address : Email address to send expiration notices"
+    echo "  -f domain file   : File with a list of domains"
+    echo "  -h               : Print this screen"
+    echo "  -s whois server  : Whois sever to query for information"
+    echo "  -q               : Don't print anything on the console"
+    echo "  -x days          : Domain expiration interval (eg. if domain_date < days)"
+    echo "  -v               : Show debug information when running script"
+    echo "  -V               : Print version of the script"
+    echo ""
+}
+
+### Evaluate the options passed on the command line
+while getopts ad:e:f:hs:qx:vV option
+do
+    case "${option}"
+    in
+        a) ALARM="TRUE";;
+        d) DOMAIN=${OPTARG};;
+        e) ADMIN=${OPTARG};;
+        f) SERVERFILE=$OPTARG;;
+        s) WHOIS_SERVER=$OPTARG;;
+        q) QUIET="TRUE";;
+        x) WARNDAYS=$OPTARG;;
+        v) VERBOSE="TRUE";;
+        V) VERSIONENABLE="TRUE";;
+        \?) usage
+        exit 1;;
+    esac
+done
+
+### Show debug information when running script
+if [ "${VERBOSE}" == "TRUE" ]
+then
+    set -x
+fi
+
+### Check to see if the whois binary exists
+if [ ! -f ${WHOIS} ]
+then
+    echo "ERROR: The whois binary does not exist in ${WHOIS} ."
+    echo "  FIX: Please modify the \$WHOIS variable in the program header."
+    exit 1
+fi
+
+### Check to make sure a date utility is available
+if [ ! -f ${DATE} ]
+then
+    echo "ERROR: The date binary does not exist in ${DATE} ."
+    echo "  FIX: Please modify the \$DATE variable in the program header."
+    exit 1
+fi
+
+### Print version of the script
+if [ "${VERSIONENABLE}" == "TRUE" ]
+then
+    printf "%-15s %-10s\n" "Script version: " "${VERSION}"
+    exit 1
+fi
+
+
+### Touch the files prior to using them
+touch ${WHOIS_TMP}
+
+### If a HOST and PORT were passed on the cmdline, use those values
+if [ "${DOMAIN}" != "" ]
+then
+    print_heading
+    check_domain_status "${DOMAIN}"
+### If a file and a "-a" are passed on the command line, check all
+### of the domains in the file to see if they are about to expire
+elif [ -f "${SERVERFILE}" ]
+then
+    print_heading
+    while read DOMAIN
+    do
+        check_domain_status "${DOMAIN}"
+
+    done < ${SERVERFILE}
+
+### There was an error, so print a detailed usage message and exit
+else
+    usage
+    exit 1
+fi
+
+# Add an extra newline
+echo
+
+### Remove the temporary files
+[ -f "${WHOIS_TMP}" ] && rm -f ${WHOIS_TMP};
+[ -f "${WHOIS_2_TMP}" ] && rm -f ${WHOIS_2_TMP};
+
+### Exit with a success indicator
+exit 0
--- scripts/expired_domains.sh
+++ scripts/expired_domains.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# v1.7.9
+# v1.8
 
 # MAIN_PATH to miejsce, w którym znajduje się główny katalog repozytorium (zakładamy, że skrypt znajduje się w katalogu o 1 niżej od głównego katalogu repozytorium)
 MAIN_PATH=$(dirname "$0")/..
 
 cd $MAIN_PATH
-patch -Np1 -i $MAIN_PATH/scripts/improve.patch
 
 for i in "$@"; do
 
@@ -60,13 +59,16 @@ sed -i "s/^www[0-9]\.//" $TEMPORARY.2
 sed -i "s/^www\.//" $TEMPORARY.2
 sort -u -o $TEMPORARY.2 $TEMPORARY.2
 
-$MAIN_PATH/scripts/domain-check-2.sh -f $TEMPORARY.2 | tee $TEMPORARY.3
+$MAIN_PATH/scripts/DSC.sh -f $TEMPORARY.2 | tee $TEMPORARY.3
 
 
 sed '/Expired/!d' $TEMPORARY.3 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
-sed '/B_blocked/!d' $TEMPORARY.3 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+sed '/Book_blocked/!d' $TEMPORARY.3 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
 sed '/Suspended/!d' $TEMPORARY.3 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
-sed '/Unknown/!d' $TEMPORARY.3 | cut -d' ' -f1 >> $TEMPORARY.4
+sed '/Removed/!d' $TEMPORARY.3 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+sed '/Free/!d' $TEMPORARY.3 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+sed '/Redemption_period/!d' $TEMPORARY.3 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+awk -F' ' '$2=="Unknown"' $TEMPORARY.3 | cut -d' ' -f1 >> $TEMPORARY.4
 
 for ips in `cat $TEMPORARY.4`
 do
@@ -87,10 +89,14 @@ done
 awk -F. '{if ($(NF-1) == "co"|| $(NF-1) == "com" || $(NF-1) == "net" || $(NF-1) == "edu" || $(NF-1) == "org" || $(NF-1) == "info" || $(NF-1) == "gov" || $(NF-1) == "biz" || $(NF-1) == "art" || $(NF-1) == "aid" || $(NF-1) == "agro" || $(NF-1) == "atm" || $(NF-1) == "auto" || $(NF-1) == "chem" || $(NF-1) == "gmina" || $(NF-1) == "gsm" || $(NF-1) == "mail" || $(NF-1) == "med" || $(NF-1) == "miasta" || $(NF-1) == "media" || $(NF-1) == "mil" || $(NF-1) == "nieruchomosci" || $(NF-1) == "nom" || $(NF-1) == "pc" || $(NF-1) == "powiat" || $(NF-1) == "priv" || $(NF-1) == "realestate" || $(NF-1) == "rel" || $(NF-1) == "sci" || $(NF-1) == "shop" || $(NF-1) == "sklep" || $(NF-1) == "sos" || $(NF-1) == "szkola" || $(NF-1) == "targi" || $(NF-1) == "tm" || $(NF-1) == "tourism" || $(NF-1) == "travel" || $(NF-1) == "turystyka" ) printf $(NF-2)"."; printf $(NF-1)"."$(NF)"\n";}' $TEMPORARY.5 >> $TEMPORARY.6
 sort -u -o $TEMPORARY.6 $TEMPORARY.6
 
-$MAIN_PATH/scripts/domain-check-2.sh -f $TEMPORARY.6 | tee $TEMPORARY.7
+$MAIN_PATH/scripts/DSC.sh -f $TEMPORARY.6 | tee $TEMPORARY.7
 sed '/Expired/!d' $TEMPORARY.7 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
-sed '/B_blocked/!d' $TEMPORARY.7 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
-sed '/Unknown/!d' $TEMPORARY.7 | cut -d' ' -f1 >> $TEMPORARY.8
+sed '/Book_blocked/!d' $TEMPORARY.7 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+sed '/Suspended/!d' $TEMPORARY.7 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+sed '/Removed/!d' $TEMPORARY.7 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+sed '/Free/!d' $TEMPORARY.7 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+sed '/Redemption_period/!d' $TEMPORARY.7 | cut -d' ' -f1 >> $MAIN_PATH/expired-domains/$FILTERLIST-expired.txt
+awk -F' ' '$2=="Unknown"' $TEMPORARY.7 | cut -d' ' -f1 >> $TEMPORARY.8
 
 for ips in `cat $TEMPORARY.8`
 do
