0edfdbcbb72607d841642fdddefe9d98df48e135
hawkeye116477 <hawkeye116477@gmail.com>
2019-08-13 18:34:14 +0200

    Fix na `*` dla FOPa  [ci skip]
    Normalnie FOP wstawia odstęp między elementem a `>`, w przypadku `*` będzie olewał sprawę. Tak musi być, bo inaczej jak jest `:*`, to wtedy usuwa `*`.

--- scripts/FOP.py
+++ scripts/FOP.py
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <http://www.gnu.org/licenses/>."""
 # FOP version number
-VERSION = 3.13
+VERSION = 3.14
 
 # Import the key modules
 import collections, filecmp, os, re, subprocess, sys
@@ -39,7 +39,7 @@ from urllib.parse import urlparse
 # Compile regular expressions to match important filter parts (derived from Wladimir Palant's Adblock Plus source code)
 ELEMENTDOMAINPATTERN = re.compile(r"^([^\/\*\|\@\"\!]*?)#?\$?\$?\??\@?#")
 FILTERDOMAINPATTERN = re.compile(r"(?:\$|\,)domain\=([^\,\s]+)$")
-ELEMENTPATTERN = re.compile(r"^([^\/\*\|\@\"\!]*?)(\$?\$?#?\@?#\+?)([^{}]+)$")
+ELEMENTPATTERN = re.compile(r"^([^\/\*\|\@\"\!]*?)(\$?\$?#?\@?#\+?)([^{\*}]+)$")
 OPTIONPATTERN = re.compile(r"^(.*)\$(~?[\w\-]+(?:=[^,\s]+)?(?:,~?[\w\-]+(?:=[^,\s]+)?)*)$")
 
 # Compile regular expressions that match element tags and pseudo classes and strings and tree selectors; "@" indicates either the beginning or the end of a selector
