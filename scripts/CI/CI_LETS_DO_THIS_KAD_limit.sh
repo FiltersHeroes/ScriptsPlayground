c6a20495f1e72781e0ad7acb670accbaa64ef7ed
hawkeye116477 <hawkeye116477@gmail.com>
2020-04-03 12:35:50 +0200

    Update CI_LETS_DO_THIS_KAD_limit.sh
    [ci skip]

--- scripts/CI/CI_LETS_DO_THIS_KAD_limit.sh
+++ scripts/CI/CI_LETS_DO_THIS_KAD_limit.sh
@@ -8,6 +8,8 @@ cut -d' ' -f1 ./expired-domains/KAD-unknown_limit.txt >> ./KAD.txt
 
 rm -rf ./expired-domains/KAD-unknown_limit.txt
 
+touch ./expired-domains/KAD-unknown_limit.txt
+
 sed -i "s|^|0.0.0.0 |" ./KAD.txt
 
 NO_RM="true" ./scripts/ECODFF.sh ./KAD.txt
