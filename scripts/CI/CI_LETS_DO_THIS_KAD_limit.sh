f85ae50d6076f7dab4bd598b8f2ae2db772b5913
hawkeye116477 <hawkeye116477@gmail.com>
2020-04-03 12:29:27 +0200

    Small fix for CI script [ci skip]

--- scripts/CI/CI_LETS_DO_THIS_KAD_limit.sh
+++ scripts/CI/CI_LETS_DO_THIS_KAD_limit.sh
@@ -6,6 +6,8 @@ cd $sciezka/../..
 
 cut -d' ' -f1 ./expired-domains/KAD-unknown_limit.txt >> ./KAD.txt
 
+rm -rf ./expired-domains/KAD-unknown_limit.txt
+
 sed -i "s|^|0.0.0.0 |" ./KAD.txt
 
 NO_RM="true" ./scripts/ECODFF.sh ./KAD.txt
