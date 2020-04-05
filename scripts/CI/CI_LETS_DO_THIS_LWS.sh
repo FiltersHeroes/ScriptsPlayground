aa6da099390dd009e1edafa88e2b1cadad84d78f
hawkeye116477 <hawkeye116477@gmail.com>
2020-04-05 14:00:53 +0200

    Fix LWS script
    [ci skip]

--- scripts/CI/CI_LETS_DO_THIS_LWS.sh
+++ scripts/CI/CI_LETS_DO_THIS_LWS.sh
@@ -9,19 +9,21 @@ cd "$MAIN_PATH" || exit
 rm -r "$MAIN_PATH"/novelties/podejrzane_LWS.txt
 LWS=$SCRIPT_PATH/LWS.temp
 python3 "$SCRIPT_PATH"/../findSuspiciousDomains_LWS.py >> "$LWS"
-sed -i -r "s|^|\|\||" "$LWS"
-sed -i -r 's|$|\^\$all|' "$LWS"
-wget https://raw.githubusercontent.com/PolishFiltersTeam/KAD/master/sections/podejrzane_inne_oszustwa.txt
-sed -i -r "s/^[|][|]www[0-9]\.//" ./podejrzane_inne_oszustwa.txt
-sed -i -r "s/^[|][|]www\.//" ./podejrzane_inne_oszustwa.txt
-#sed -i -r "s/^[|][|]//" ./podejrzane_inne_oszustwa.txt
-#sed -i -r "s|^|\|\||" ./podejrzane_inne_oszustwa.txt
-sort -u -o ./podejrzane_inne_oszustwa.txt ./podejrzane_inne_oszustwa.txt
+wget https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt
+pcregrep -o1 '^.*?0.0.0.0 (.*)' ./KADhosts.txt >> ./KADhosts_temp.txt
+sed -i 's/^www.//g' "$LWS"
+rm -rf ./KADhosts.txt
+mv ./KADhosts_temp.txt ./KADhosts.txt
+sort -u -o ./KADhosts.txt ./KADhosts.txt
 sort -u -o "$LWS" "$LWS"
-comm -1 -3 ./podejrzane_inne_oszustwa.txt "$LWS" >> "$LWS".2
-rm -r "$LWS"
-rm -r ./podejrzane_inne_oszustwa.txt
+comm -1 -3 ./KADhosts.txt "$LWS" >> "$LWS".2
+rm -r "$CERT"
+rm -r ./KADhosts.txt
 mv "$LWS".2 "$SCRIPT_PATH"/LWS_temp.txt
+sed -i 's/^www.//g' "$SCRIPT_PATH"/LWS_temp.txt
+sed -i -r "s|^|\|\||" "$SCRIPT_PATH"/LWS_temp.txt
+sed -i -r 's|$|\^\$all|' "$SCRIPT_PATH"/LWS_temp.txt
+sort -u -o"$SCRIPT_PATH"/LWS_temp.txt "$SCRIPT_PATH"/LWS_temp.txt
 
 NO_SC="true" ./scripts/ECODFF.sh "$SCRIPT_PATH"/LWS_temp.txt
 
@@ -47,9 +49,6 @@ fi
 if [ ! -f "$MAIN_PATH/novelties/podejrzane_LWS.txt" ]; then
     mv "$SCRIPT_PATH"/LWS_temp.txt "$MAIN_PATH"/novelties/podejrzane_LWS.txt
 fi
-
-sed -i -r "s|^|\|\||" "$MAIN_PATH"/novelties/podejrzane_LWS.txt
-sed -i -r 's|$|\^\$all|' "$MAIN_PATH"/novelties/podejrzane_LWS.txt
 
 if [ -f "$EXPIRED" ]; then
     rm -r "$EXPIRED"
