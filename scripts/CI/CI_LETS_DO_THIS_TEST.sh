2d3ee6279773d14b69bad63bed0fc31109eae848
hawkeye116477 <hawkeye116477@gmail.com>
2020-05-24 20:27:10 +0200

    Poprawka

--- scripts/CI/CI_LETS_DO_THIS_TEST.sh
+++ scripts/CI/CI_LETS_DO_THIS_TEST.sh
@@ -6,4 +6,5 @@ cd $sciezka/../..
 
 wget https://raw.githubusercontent.com/PolishFiltersTeam/ScriptsPlayground/master/expired-domains/KAD-unknown.txt
 
+sed -i 's/ 000//g' ./KAD-unknown.txt
 ./scripts/DSC.sh -f ./KAD-unknown.txt
