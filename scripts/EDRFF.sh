21a11a2a1f3e1c4e36583b9b619ab81238d14259
hawkeye116477 <hawkeye116477@gmail.com>
2020-12-27 22:43:10 +0100

    Add choice to remove domains only with specific TLD
    [ci skip]

--- scripts/EDRFF.sh
+++ scripts/EDRFF.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
 # Expired Domains Remover For Filterlists
-# v1.0
-# Usage: EDRFF.sh pathToSections listOfExpiredDomains.txt
+# v1.1
+# Usage: EDRFF.sh pathToSections listOfExpiredDomains.txt TLD (optional)
 
 SECTIONS_DIR=$1
 
+removeE() {
+    grep -v -f "$2" "$1" > "$1".temp
+    mv "$1".temp "$1"
+}
+
 if [ -d "${SECTIONS_DIR}" ]; then
     for file in "${SECTIONS_DIR}"/*.txt; do
-        grep -v -f $2 $file > $file.temp
-        mv $file.temp $file
+        # Remove domains only with specific TLD
+        if [ "$3" ]; then
+            grep ".$3$" "$2" > "$file".temp_"$3"
+            removeE "$file" "$file".temp_"$3"
+            rm -rf "$file".temp_"$3"
+        else
+            removeE "$file" "$2"
+        fi
     done
 fi
