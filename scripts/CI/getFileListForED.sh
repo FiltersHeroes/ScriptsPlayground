bd71a00dea2ec2222bdc8803c8b9a7ba1408c3ee
hawkeye116477 <hawkeye116477@gmail.com>
2021-04-10 13:48:20 +0200

    Fix GL CI artifacts

--- scripts/CI/getFileListForED.sh
+++ scripts/CI/getFileListForED.sh
@@ -2,6 +2,7 @@
 
 for i in "$@"; do
     echo "expired-domains/$i-expired.txt"
+    echo "expired-domains/$i-parked.txt"
     echo "expired-domains/$i-unknown.txt"
     echo "expired-domains/$i-unknown_limit.txt"
     echo "expired-domains/$i-unknown_no_internet.txt"
