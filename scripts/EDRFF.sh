5d617d12c0ddb97766041958d5dac8de4ba60247
hawkeye116477 <hawkeye116477@gmail.com>
2021-04-11 11:54:39 +0200

    Update Expired Domains Remover For Filterlists
    * Dodano usuwanie linii zawierających wzorzec  „||domena” 
    * Dodano usuwanie wyrazów według wzorcu  „domena|”

--- scripts/EDRFF.sh
+++ scripts/EDRFF.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
 # Expired Domains Remover For Filterlists
-# v1.1 (beta)
+# v1.2 (beta)
 # Usage: EDRFF.sh pathToSections listOfExpiredDomains.txt TLD (optional)
+
+SCRIPT_PATH=$(dirname "$(realpath -s "$0")")
+
+if [ -z "$MAIN_PATH" ]; then
+    MAIN_PATH=$(git -C "$SCRIPT_PATH" rev-parse --show-toplevel)
+fi
 
 SECTIONS_DIR=$1
 
+TMP_DIR="$MAIN_PATH"/tmp
+
+if [ ! -d "$TMP_DIR" ]; then
+    mkdir -p "$TMP_DIR"
+fi
+
 removeE() {
-    grep -v -f "$2" "$1" > "$1".temp
+    cp "$2" "$TMP_DIR"/expired1.tmp
+    cp "$2" "$TMP_DIR"/expired2.tmp
+    sed -i "s/^/||/" "$TMP_DIR"/expired1.tmp
+    sed -i "s/$/|/" "$TMP_DIR"/expired2.tmp
+    grep -v -f "$TMP_DIR"/expired1.tmp "$1" > "$1".temp
     mv "$1".temp "$1"
+    sed -i "$(sed 's:.*:s/&//ig:' "$TMP_DIR"/expired2.tmp)" "$1"
+    rm -rf "$TMP_DIR"/*
 }
 
 if [ -d "${SECTIONS_DIR}" ]; then
@@ -23,3 +41,5 @@ if [ -d "${SECTIONS_DIR}" ]; then
         fi
     done
 fi
+
+rm -rf "$TMP_DIR"
