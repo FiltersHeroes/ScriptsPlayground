32e693fd986cb5ad819ad197a6c628f0d973e403
hawkeye116477 <hawkeye116477@gmail.com>
2021-11-06 21:56:53 +0100

    Update copyright

--- scripts/VICHS.sh
+++ scripts/VICHS.sh
@@ -5,7 +5,7 @@
 
 # MIT License
 
-# Copyright (c) 2021 Polish Filters Team
+# Copyright (c) 2021 Filters Heroes
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
