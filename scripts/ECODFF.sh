cd67e0f2d4fa06c240125c866fb18266bcc4a21e
krystian3w <35370833+krystian3w@users.noreply.github.com>
2023-01-03 22:04:51 +0100

    Update ECODFF.sh

--- scripts/ECODFF.sh
+++ scripts/ECODFF.sh
@@ -5,7 +5,7 @@
 
 # MIT License
 
-# Copyright (c) 2022 Filters Heroes
+# Copyright (c) 2023 Filters Heroes
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
