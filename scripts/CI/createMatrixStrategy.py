40b640a2a40c03f92f3a2f1e63c5d2308033a07f
hawkeye116477 <hawkeye116477@gmail.com>
2023-09-16 17:22:01 +0200

    Update scripts

---
+++ scripts/CI/createMatrixStrategy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# pylint: disable=C0103
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+import os
+
+KAD_matrix_strategy = []
+for i in range(1, int(os.getenv("NUMBER_OF_KAD_JOBS")) + 1):
+    KAD_matrix_strategy.append(i)
+
+KADhosts_matrix_strategy = []
+for i in range(1, int(os.getenv("NUMBER_OF_KADHOSTS_JOBS")) + 1):
+    KADhosts_matrix_strategy.append(i)
+
+with open(os.getenv("GITHUB_OUTPUT"), "a") as gho:
+    print(f"KAD-strategy-matrix={KAD_matrix_strategy}", file=gho)
+    print(f"KADh-strategy-matrix={KADhosts_matrix_strategy}", file=gho)
--- scripts/CI/downloadArtifacts.py
+++ scripts/CI/downloadArtifacts.py
@@ -10,16 +10,11 @@ import shutil
 import asyncio
 import aiohttp
 
-
 pj = os.path.join
 pn = os.path.normpath
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 main_path = os.getenv("GITHUB_WORKSPACE")
-
-extract_dir = sys.argv[2]
-if not os.path.isdir(extract_dir):
-    os.mkdir(extract_dir)
 
 async def download_artifact(session, limit, name):
     async with limit:
@@ -56,7 +51,24 @@ async def download_artifacts(limit_value, names):
     async with aiohttp.ClientSession() as session:
         await asyncio.gather(*[download_artifact(session, limit, name) for name in names])
 
-artifact_names = []
-if sys.argv[1]:
-    artifact_names = os.getenv(str(sys.argv[1])).split(" ")
+artifact_names=[]
+needed_env = 2
+
+if sys.argv[1] == "KAD" and "NUMBER_OF_KAD_JOBS" in os.environ:
+    needed_env = int(os.getenv("NUMBER_OF_KAD_JOBS"))
+elif sys.argv[1] == "KADhosts" and "NUMBER_OF_KADHOSTS_JOBS" in os.environ:
+    needed_env = int(os.getenv("NUMBER_OF_KADHOSTS_JOBS"))
+
+for i in range(1, needed_env + 1):
+    if i < 10:
+        file_number = f"0{i}"
+    else:
+        file_number = i
+    artifact_names.append(f"E_{sys.argv[1]}_{file_number}")
+
+
+extract_dir = sys.argv[2]
+if not os.path.isdir(extract_dir):
+    os.mkdir(extract_dir)
+
 asyncio.run(download_artifacts(5, artifact_names))
