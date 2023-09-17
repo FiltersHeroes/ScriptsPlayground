87a4798d7cfc01b8907908de3cda97ea7004e81c
hawkeye116477 <hawkeye116477@gmail.com>
2023-09-17 22:43:03 +0200

    Fix expiredDomains action

--- scripts/CI/getArtifactNamesForED.py
+++ scripts/CI/getArtifactNamesForED.py
@@ -23,10 +23,9 @@ for i in range(1, needed_env + 1):
         file_number = i
     names.append(f"E_{sys.argv[1]}_{file_number}")
 
-env_file = os.getenv('GITHUB_ENV')
+env_file = os.getenv("GITHUB_ENV")
 with open(env_file, "a", encoding="utf-8") as env_file_c:
-    env_file_c.write(f"E_{sys.argv[1]}_NAMES=")
-    for i, file_name in enumerate(names):
-        if i > 0:
-            env_file_c.write(' ')
-        env_file_c.write(f"{file_name}")
+    env_file_c.write(f"E_{sys.argv[1]}_NAMES<<EOF\n")
+    for file_name in names:
+        env_file_c.write(f"{file_name}\n")
+    env_file_c.write("EOF\n")
