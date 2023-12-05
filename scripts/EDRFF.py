8154af8ff1ab1542106fb608f3a7b2e09615e7dd
hawkeye116477 <hawkeye116477@gmail.com>
2023-12-05 22:33:29 +0100

    Update EDRFF to v1.7.4
    * Speed up script (on example, before about 13s, now about 2s)

--- scripts/EDRFF.py
+++ scripts/EDRFF.py
@@ -3,7 +3,7 @@
 # pylint: disable=anomalous-backslash-in-string
 # pylint: disable=C0103
 # Expired Domains Remover For Filterlists
-# v1.7.3
+# v1.7.4
 # Usage: EDRFF.py pathToSections listOfExpiredDomains.txt TLD (optional) "exclude"(optional)
 
 import os
@@ -11,6 +11,7 @@ import sys
 import shutil
 import re
 from tempfile import NamedTemporaryFile
+from multiprocessing import Pool
 
 pj = os.path.join
 pn = os.path.normpath
@@ -68,16 +69,27 @@ merged_regex_list = '|'.join(regex_list)
 new_regex = re.compile(f"({merged_regex_list})")
 del regex_part_domains, regex_list, merged_regex_list
 
-for root, dirs, files in os.walk(sections_path):
-    for section in files:
-        print(f"Checking {section} ...")
-        section_file_path = pj(root, section)
-        with open(section_file_path, "r", encoding='utf-8') as section_f, NamedTemporaryFile(dir='.', delete=False, mode="w", encoding='utf-8') as f_out:
-            for line in section_f:
-                line = line.strip()
-                if line := new_regex.sub("", line):
-                    f_out.write(f"{line}\n")
-            os.rename(f_out.name, section_file_path)
+
+def remove_domains(line):
+    line = line.strip()
+    if line := new_regex.sub("", line):
+        line = f"{line}\n"
+    return line
+
+
+with Pool() as p:
+    for root, dirs, files in os.walk(sections_path):
+        for section in files:
+            section_file_path = pj(root, section)
+            section = os.path.basename(section_file_path)
+            print(f"Checking {section} ...")
+            with open(section_file_path, "r", encoding='utf-8') as section_f:
+                results = p.map(remove_domains, section_f)
+            with open(section_file_path, "w", encoding='utf-8') as f_out:
+                for line in results:
+                    f_out.write(line)
+    p.close()
+    p.join()
 
 os.chdir(main_path)
 os.rmdir(temp_path)
