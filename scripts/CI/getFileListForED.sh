a43966b76373a2e83312b3a4cdeeb8f53c84dddc
hawkeye116477 <hawkeye116477@gmail.com>
2025-06-15 14:02:56 +0200

    Fix online handling

--- scripts/CI/getFileListForED.sh
+++ scripts/CI/getFileListForED.sh
@@ -5,4 +5,5 @@ for i in "$@"; do
     echo "expired-domains/$i-parked.txt"
     echo "expired-domains/$i-unknown.txt"
     echo "expired-domains/$i-unknown_limit.txt"
+    echo "expired-domains/$i-online"
 done
